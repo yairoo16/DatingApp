71512baf5cd80564fa1a65ab537b5e657b0b301d
Benyair Orellana <orellanab@michigan.gov>
2018-07-25 16:02:14 -0400

    End of Section 8

--- DatingApp.API/DTOs/PhotosForDetailedDTO.cs
+++ DatingApp.API/DTOs/PhotosForDetailedDTO.cs
@@ -1,15 +1,13 @@
 using System;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.DTOs
 {
-    public class Photo
+    public class PhotosForDetailedDTO
     {
         public int Id {get; set;}
         public string Url {get; set;}
         public string Description {get; set;}
         public DateTime DateAdded {get; set;}
         public bool IsMain {get; set;}
-        public User User {get; set;}
-        public int UserId {get; set;}
     }
 }
