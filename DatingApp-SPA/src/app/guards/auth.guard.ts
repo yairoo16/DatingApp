45b7dda5f78b38a1141cae8ed9c689f83707abfe
Benyair Orellana <ben.orellana@hotmail.com>
2018-07-24 22:16:30 -0400

    End of Section 7

---
+++ DatingApp-SPA/src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { AlertifyService } from '../service/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router,
+    private alertify: AlertifyService) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+    this.alertify.error('You shall not pass!!!');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
